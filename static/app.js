cb7dc2ba083a0b541c041a876725df3923ad2a98
David Hollema <david.hollema@woolpert.com>
2020-07-15 14:24:25 -0600

    Highlight order and geofence on hover.

--- static/app.js
+++ static/app.js
@@ -7,65 +7,47 @@ const socket = io();
 //
 
 let map = null;
-let orderMarkers = [];
-const colors = ['#bbdefb', '#2196f3', '#0d47a1', 'grey'];
-const geoFences = [];
 const stores = [];
+let geofencesLayer;
+const geofenceColorRamp = {
+  600: '#bbdefb',
+  300: '#2196f3',
+  120: '#0d47a1',
+};
+
+let ordersLayer;
 const orderGroups = [
   {
     minutes: '2',
     range: 120,
-    color: colors[2],
-    locations: [],
+    color: geofenceColorRamp[120],
+    orders: [],
   },
   {
     minutes: '5',
     range: 300,
-    color: colors[1],
-    locations: [],
+    color: geofenceColorRamp[300],
+    orders: [],
   },
   {
     minutes: '10',
     range: 600,
-    color: colors[0],
-    locations: [],
+    color: geofenceColorRamp[600],
+    orders: [],
   },
   {
     minutes: '>10',
     range: 601,
-    locations: [],
+    color: 'grey',
+    orders: [],
   },
 ];
 
 //
 //
-// Helper Functions
+// UI Events
 //
 //
-
-function getColor(range) {
-  switch (range) {
-    case 120:
-      return colors[2];
-    case 300:
-      return colors[1];
-    default:
-      return colors[0];
-  }
-}
-
-function loadMap() {
-  const location = new google.maps.LatLng(1, 1);
-
-  const options = {
-    center: location,
-    zoom: 0,
-    mapId: '6a9eb25b0d956d9d',
-    gestureHandling: 'greedy', // disable the stupid default CTRL required to zoom
-  };
-
-  map = new google.maps.Map(document.getElementById('map-container'), options);
-}
 
 function renderStore(store) {
   const template = document.getElementById('store-template').innerHTML;
@@ -91,36 +73,12 @@ function renderStore(store) {
   });
 }
 
-function setExtentToStores() {
-  const bounds = new google.maps.LatLngBounds();
-  stores.forEach((store) => {
-    const latlng = new google.maps.LatLng(store.location.latitude, store.location.longitude);
-    bounds.extend(latlng);
-  });
-  map.fitBounds(bounds);
-}
-
-function alertOrder(callback, delay, reps, orderId) {
-  let x = 0;
-  const intervalId = window.setInterval(() => {
-    callback(x, orderId);
-    if (++x === reps) {
-      window.clearInterval(intervalId);
-    }
-  }, delay);
-}
-
-//
-//
-// UI Events
-//
-//
-
 function onGetOrders(storeName) {
   const store = stores.find(store => store.name === storeName);
-  console.log('Getting orders for ', store.name);
+  console.log('Getting orders for', store.name);
   socket.emit('get geofences', store.name);
 
+  document.getElementById('store-page-header').innerHTML = '';
   document.getElementById('stores').innerHTML = '';
   // render store page header
   const template = document.getElementById('store-page-header-template').innerHTML;
@@ -141,26 +99,44 @@ function onNavStores(id) {
   setExtentToStores();
 }
 
-function onShowOrder(id) {
-  console.log('orderId', id);
-  // if order and store distance is <100 ft, zoom to th
-  // zoom to extent of order and store
+function onHoverOrder(id) {
+  ordersLayer.revertStyle();
+  const feature = ordersLayer.getFeatureById(id);
+  if (feature) {
+    ordersLayer.overrideStyle(feature, {
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 6,
+        fillColor: '#bf360c',
+        fillOpacity: 0.9,
+        strokeColor: '#fcfda1',
+        strokeWeight: 3.0,
+      }
+    });
+  }
+}
+
+function onHoverOrderGroup(id) {
+  geofencesLayer.revertStyle();
+  const feature = geofencesLayer.getFeatureById(id);
+  if (feature) {
+    geofencesLayer.overrideStyle(feature, {
+      fillOpacity: 0.75,
+      strokeColor: '#0d47a1',
+      strokeWeight: 1.0,
+    });
+  }
 }
 
 function onShowRange(range) {
   // recenter the map to isochrone extents
-  let index = 0;
-  if (parseInt(range) === 120) {
-    index = 2;
-  } else if (parseInt(range) === 300) {
-    index = 1;
+  let geofence = geofencesLayer.getFeatureById(range);
+  if (!geofence) {
+    geofence = geofencesLayer.getFeatureById(600);
   }
-
   const bounds = new google.maps.LatLngBounds();
-  geoFences[index].forEach((feature) => {
-    feature.getGeometry().forEachLatLng((latlng) => {
-      bounds.extend(latlng);
-    });
+  geofence.getGeometry().forEachLatLng((latlng) => {
+    bounds.extend(latlng);
   });
   map.fitBounds(bounds);
 }
@@ -190,49 +166,48 @@ socket.on('connected', (msg) => {
 
 socket.on('orders', (msg) => {
   console.log('orders', msg);
-
-  if (msg == null || msg.length === 0) {
-    // alert no orders for this store
+  let orders = msg;
+  orders = orders.filter(order => order.latestEvent);
+  if (orders == null || orders.length === 0) {
+    console.log('No orders with latest events.');
   }
 
   // clear out existing orders and data
   const previousOrders = [];
   orderGroups.forEach((group) => {
-    group.locations.forEach((item) => {
+    group.orders.forEach((item) => {
       previousOrders.push(item);
     });
-    group.locations = [];
+    group.orders = [];
   });
+  ordersLayer.forEach(feature => ordersLayer.remove(feature));
+  ordersLayer.setMap(null);
 
-  // replace list of stores with orders
-  // show the order driver locations on the map
-  orderMarkers.forEach((marker) => {
-    marker.setMap(null);
-  });
-  orderMarkers = [];
-  msg.forEach((order) => {
-    if (order.latestEvent && !order.latestEvent.innerGeofence) {
-      orderGroups[3].locations.push(order);
-    } else if (order.latestEvent) {
-      switch (order.latestEvent.innerGeofence.range) {
-        case 120:
-          orderGroups[0].locations.push(order);
-          break;
-        case 300:
-          orderGroups[1].locations.push(order);
-          break;
-        case 600:
-          orderGroups[2].locations.push(order);
-          break;
-        default:
-          orderGroups[3].locations.push(order);
-      }
+  // show the order locations on the map
+  for (let i = 0; i < orders.length; i++) {
+    const order = orders[i];
+    if (!order.latestEvent.innerGeofence) {
+      orderGroups[3].orders.push(order);
+      continue;
     }
-  });
+    switch (order.latestEvent.innerGeofence.range) {
+      case 120:
+        orderGroups[0].orders.push(order);
+        break;
+      case 300:
+        orderGroups[1].orders.push(order);
+        break;
+      case 600:
+        orderGroups[2].orders.push(order);
+        break;
+      default:
+        orderGroups[3].orders.push(order);
+    }
+  }
 
-  // sort them by most recently received
-  orderGroups.forEach((list) => {
-    list.locations.sort((a, b) => {
+  // sort orders by most recently received
+  orderGroups.forEach((group) => {
+    group.orders.sort((a, b) => {
       const aDate = new Date(a.latestEvent.eventTimestamp * 1000);
       const bDate = new Date(b.latestEvent.eventTimestamp * 1000);
       return aDate - bDate;
@@ -241,22 +216,22 @@ socket.on('orders', (msg) => {
 
   // render the isochrone grouping headers
   document.getElementById('stores').innerHTML = '';
-  orderGroups.forEach((list) => {
+  orderGroups.forEach((group) => {
     const template = document.getElementById('order-group-template').innerHTML;
     const rendered = Mustache.render(template, {
       range: {
-        id: list.range,
-        color: list.color,
-        time: list.minutes,
+        id: group.range,
+        color: group.color,
+        time: group.minutes,
       },
     });
     document.getElementById('stores').innerHTML += rendered;
   });
 
-  orderGroups.forEach((list) => {
-    // indicate to user no orders for this isochrone range
+  orderGroups.forEach((group) => {
+    // indicate no orders for this isochrone range
     let emptyGroup = true;
-    list.locations.forEach((order) => {
+    group.orders.forEach((order) => {
       if (!order.status.includes('closed')) {
         emptyGroup = false;
       }
@@ -264,52 +239,44 @@ socket.on('orders', (msg) => {
     if (emptyGroup) {
       const template = document.getElementById('order-empty-template').innerHTML;
       const rendered = Mustache.render(template);
-      document.getElementById(`range-${list.range}`).innerHTML += rendered;
+      document.getElementById(`range-${group.range}`).innerHTML += rendered;
     }
 
     // render each order
-    const openOrderLocations = list.locations.filter(location => !location.status.includes('closed'));
-    openOrderLocations.forEach((location) => {
-      const lastPing = new Date(location.latestEvent.eventTimestamp * 1000);
+    const openOrders = group.orders.filter(order => !order.status.includes('closed'));
+    openOrders.forEach(order => {
+      // reformat absolute timestamp to an age (seconds ago, minutes ago)
+      const lastPing = new Date(order.latestEvent.eventTimestamp * 1000);
       const secondsAgo = (new Date() - lastPing) / 1000;
       if (secondsAgo < 5) {
-        location.date = 'now';
+        order.date = 'now';
       } else if (secondsAgo / 60 < 1) {
-        location.date = Math.round(secondsAgo) + ' sec ago';
+        order.date = Math.round(secondsAgo) + ' sec ago';
       } else if (secondsAgo / 3600 < 1) {
-        location.date = (secondsAgo / 60).toFixed(1) + ' min ago';
+        order.date = (secondsAgo / 60).toFixed(1) + ' min ago';
       } else {
-        location.date = (secondsAgo / 3600).toFixed(1) + ' hrs ago';
+        order.date = (secondsAgo / 3600).toFixed(1) + ' hrs ago';
       }
-      const loc = {
-        lat: location.latestEvent.eventLocation.latitude,
-        lng: location.latestEvent.eventLocation.longitude,
-      };
 
-      // create a map marker
-      orderMarkers.push(new google.maps.Marker({
-        position: loc,
-        map,
-        icon: {
-          path: google.maps.SymbolPath.CIRCLE,
-          scale: 5,
-          fillColor: '#bf360c',
-          fillOpacity: 0.9,
-          strokeWeight: 0.5,
-        },
-      }));
+      // add order to map
+      const orderLocation = {
+        lat: order.latestEvent.eventLocation.latitude,
+        lng: order.latestEvent.eventLocation.longitude,
+      };
+      const feature = new google.maps.Data.Feature({ geometry: orderLocation, id: order.orderId });
+      ordersLayer.add(feature);
 
       // render the order card
       const template = document.getElementById('order-template').innerHTML;
-      const rendered = Mustache.render(template, { order: location });
-      document.getElementById(`range-${list.range}`).innerHTML += rendered;
+      const rendered = Mustache.render(template, { order: order });
+      document.getElementById(`range-${group.range}`).innerHTML += rendered;
 
       // alert the user as the order enters the inner most isochrone
-      previousOrders.forEach((order) => {
-        if (order.orderId === location.orderId
-          && order.latestEvent && order.latestEvent.innerGeofence
-          && order.latestEvent.innerGeofence.range === 300
-          && list.range === 120) {
+      previousOrders.forEach((previousOrder) => {
+        if (previousOrder.orderId === order.orderId
+          && previousOrder.latestEvent && previousOrder.latestEvent.innerGeofence
+          && previousOrder.latestEvent.innerGeofence.range === 300
+          && group.range === 120) {
           // callback function for flashing the order
           alertOrder((count, orderId) => {
             const orderElement = document.getElementById(`order-${orderId}`);
@@ -318,51 +285,37 @@ socket.on('orders', (msg) => {
             } else {
               orderElement.classList.remove('flash');
             }
-          }, 400, 15, order.orderId); // ms delay, how many times, order id
+          }, 400, 15, previousOrder.orderId); // ms delay, how many times, order id
         }
       });
     });
+    ordersLayer.setMap(map);
   });
 });
 
 socket.on('geofences', (msg) => {
-  if (!msg || !msg.length) {
-    return;
-  }
-  if (geoFences.length > 0) {
-    // don't recreate them
-    return;
-  }
-  console.log('geofences', msg);
-  const bounds = new google.maps.LatLngBounds();
-  const layers = [];
-  const shapes = [];
-  msg.reverse(); // assume API returns outter most isochrone at index 0
-  msg.forEach(() => {
-    layers.push(new google.maps.Data({ map }));
-  });
-  layers.forEach((layer, i) => {
-    let { shape } = msg[i];
-    if (i < msg.length - 1) {
-      shape = turf.difference(msg[i].shape, msg[i + 1].shape);
-    }
-    shapes.push(shape);
-    layer.addGeoJson(shape, { idPropertyName: `isochrone-${i}` });
-    layer.id = i;
-    layer.setStyle({
-      fillColor: colors[i],
-      title: msg[i].range,
-      strokeWeight: 0.1,
-    });
-    // recenter the map to isochrone extents
-    layer.forEach((feature) => {
-      feature.getGeometry().forEachLatLng((latlng) => {
-        bounds.extend(latlng);
-      });
-    });
-    geoFences.push(layer);
-  });
+  const geofences = msg;
+  geofencesLayer.forEach(feature => geofencesLayer.remove(feature));
+  geofencesLayer.setMap(null);
 
+  if (!geofences || !geofences.length) {
+    return;
+  }
+  console.log('geofences', geofences);
+  const bounds = new google.maps.LatLngBounds();
+  geofences.reverse(); // assume API returns outter most isochrone at index 0
+
+  geofencesLayer.setMap(map);
+  for (let i = 0; i < geofences.length; i++) {
+    let { shape: geofenceRing } = geofences[i];
+    if (i < geofences.length - 1) {
+      geofenceRing = turf.difference(geofences[i].shape, geofences[i + 1].shape);
+    }
+    const feature = geofencesLayer.addGeoJson(geofenceRing, { idPropertyName: 'value' })[0];
+    feature.getGeometry().forEachLatLng((latlng) => {
+      bounds.extend(latlng);
+    });
+  }
   map.fitBounds(bounds);
 });
 
@@ -370,3 +323,63 @@ socket.on('closed', (msg) => {
   // nothing to do on the client
   console.log('I closed', msg);
 });
+
+//
+//
+// Helper Functions
+//
+//
+
+function loadMap() {
+  const location = new google.maps.LatLng(1, 1);
+
+  const options = {
+    center: location,
+    zoom: 0,
+    mapId: '6a9eb25b0d956d9d',
+    gestureHandling: 'greedy', // disable the stupid default CTRL required to zoom
+  };
+
+  map = new google.maps.Map(document.getElementById('map-container'), options);
+
+  geofencesLayer = new google.maps.Data();
+  geofencesLayer.setStyle((feature) => {
+    return {
+      fillColor: geofenceColorRamp[feature.getId()],
+      strokeWeight: 0.1
+    }
+  });
+
+  ordersLayer = new google.maps.Data();
+  ordersLayer.setStyle((feature) => {
+    return {
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 5,
+        fillColor: '#bf360c',
+        fillOpacity: 0.9,
+        strokeColor: '#ffffff',
+        strokeWeight: 0.5,
+      }
+    };
+  });
+}
+
+function setExtentToStores() {
+  const bounds = new google.maps.LatLngBounds();
+  stores.forEach((store) => {
+    const latlng = new google.maps.LatLng(store.location.latitude, store.location.longitude);
+    bounds.extend(latlng);
+  });
+  map.fitBounds(bounds);
+}
+
+function alertOrder(callback, delay, reps, orderId) {
+  let x = 0;
+  const intervalId = window.setInterval(() => {
+    callback(x, orderId);
+    if (++x === reps) {
+      window.clearInterval(intervalId);
+    }
+  }, delay);
+}
